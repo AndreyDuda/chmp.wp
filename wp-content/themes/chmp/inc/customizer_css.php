ba6f1c4971d3f8bd4692b7b6502a24f837bac383
AndreyDuda <AndreyDuda88@gmail.com>
2019-05-09 20:02:37 +0300

    full template

--- wp-content/themes/chmp/inc/customizer_css.php
+++ wp-content/themes/chmp/inc/customizer_css.php
@@ -1,19 +1,12 @@
 <style>
 	body{
-		background: <?php echo get_theme_mod('body_background') ?>;
-		font-family: "Lato";
+		font-family: 'Open Sans',sans-serif;
 		font-weight: 300;
 		font-size: 16px;
-		color:<?php echo get_theme_mod('text_color_body') ?>;
-		
-		-webkit-font-smoothing: antialiased;
-		-webkit-overflow-scrolling: touch;
-		
+		background: #fdf6ea;
 		margin-top: 70px;
 	}
-	
 	.navbar {
-		font-family: "Lato";
 		font-weight: 600;
 		font-size: 18px;
 	}
@@ -21,66 +14,416 @@
 	h1, h2, h3, h4, h5, h6 {
 		font-family: "Releway";
 		font-weight: 300;
-		color: <?php echo get_theme_mod('h_color') ?>;
+		position: relative;
 	}
-	
 	p {
 		line-height: 28px;
 		margin-bottom: 25px;
 	}
-	
 	.centered {
 		text-align: center;
 	}
-	
 	a {
-		color: <?php echo get_theme_mod('nav_color') ?>;
+		color: #000;
 	}
 	
 	a:hover, a:focus {
-		color: <?php echo get_theme_mod('nav_color_hover') ?>;
-		background-color: <?php echo get_theme_mod('nav_background') ?> ;
 		text-decoration: none;
 		outline: 0;
+		color: #81164e;
 	}
-	
+	.social {
+		display: flex;
+		justify-content: center;
+	}
+	.social i {
+		font-size: 30px;
+		margin-right: 7px;
+	}
 	.navbar-invers {
-		background: <?php echo get_theme_mod('nav_background') ?> ;
-		bprder-color: <?php echo get_theme_mod('nav_background') ?> ;
+		background: #fdf6ea;
 	}
-	
-	.navbar-invers .navbar-nav > .active > a {
-		background-color: <?php echo get_theme_mod('nav_background') ?> ;
-		border-bottom: 1px solid <?php echo get_theme_mod('nav_color') ?>;
-	}
-	
-	.navbar-invers .navbar-nav > li > a:hover {
-		background-color: <?php echo get_theme_mod('nav_background') ?> ;
-	}
-	
 	.navbar-invers .navbar-brand {
-		color: <?php echo get_theme_mod('logo_text_color') ?>;
 		font-weight: bolder;
 		font-size: 18px;
 		letter-spacing: 1px;
 	}
-	.navbar-toggle .icon-bar {
-		background: <?php echo get_theme_mod('nav_color') ?>;
-		border: 1px solid <?php echo get_theme_mod('nav_color') ?>;
-	}
-	
-	#wrap {
-		background: url(<?php echo get_template_directory_uri().'/assets/images/im.jpeg' ?>);
-		background-attachment: relarive;
-		background-position: center;
+	#headerwrap{
 		width: 100%;
 		background-size: 100%;
-		-webkit-background-size: cover;
-		-moz-background-size: cover;
-		-o-background-size: cover;
-		background-size: cover;
 		display: block;
-		height: 600px;
+		height: max-content;
+		overflow: hidden;
+		position: initial;
+	}
+	.icon-bar {
+		background: #000;
+	}
+	h2{
+		margin-bottom: 40px;
+	}
+	h2+p {
+		border: none;
+		font-size: 18px;
+		font-style: italic;
+		line-height: 32px;
+		color: gray;
+	}
+	h2:after {
+		content: '';
+		display: block;
+		width: 40px;
+		height: 3px;
+		background: #81164e;
+		position: absolute;
+		bottom: -10px;
+		left: 50%;
+		transform: translateX(-50%);
+	}
+	section {
+		padding: 50px 0;
+		text-align: center;
+	}
+	.section-2{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+	}
+	.section-3 {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		position: relative;
+	}
+	.section-3 .bg {
+		background: url(<?php echo get_template_directory_uri().'/assets/images/img-2.jpg' ?>);
+		background-size: 100%;
+		background-position: center;
+		background-attachment: fixed;
+		position: absolute;
+		top: 0;
+		left: 0;
+		bottom: 0;
+		right: 0;
+		filter: brightness(30%);
+		z-index: -1;
+	}
+	
+	.section-3 h2 {
+		color: #fff;
+	}
+	.section-3 .image {
+		background: url(<?php echo get_template_directory_uri().'/assets/images/img-3.jpg' ?>);
+		max-width: 1000px;
+		margin: 0 auto 50px;
+		-webkit-background-size: cover;
+		background-size: cover;
+		height: 700px;
+	}
+	.section-3 .list {
+		display: flex;
+		justify-content: space-between;
+		flex-wrap: wrap;
+		margin-bottom: 50px;
+	}
+	.section-3 .icon {
+		width: 50px;
+		height: 50px;
+		background: #43a898;
+		margin-right: 10px;
+		text-align: center;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.section-3 .icon i {
+		font-size: 35px;
+		color: #fff;
+	}
+	.section-3 .list-item {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 50%;
+		margin-bottom: 20px;
+		
+	}
+	.section-3 .list-item .text {
+		color: #fff;
+		font-weight: bold;
+	}
+	
+	
+	.section-4 .blocks {
+		display: flex;
+		flex-direction: column;align-items: center;
+	}
+	.section-4 .block {
+		width: 300px;
+		padding: 5px 30px;
+		font-weight: bold;
+		font-size: 24px;
+		border: 2px solid #81164e;
+		border-radius: 10px;
+		margin-bottom: 10px;
+		box-shadow: 0 0 15px rgba(0,0,0,.25)
+	}
+	.section-4 .row {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	
+	
+	.progress-block {
+		margin-top: 20px;
+	}
+	.progress {
+		margin: 0 auto;
+		width: 400px;
+	}
+	
+	.progress {
+		padding: 2px;
+		background: rgba(0, 0, 0, 0.25);
+		border-radius: 6px;
+		box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.25), 0 1px rgba(255, 255, 255, 0.08);
+	}
+	
+	.progress-bar {
+		background-color: #f2b01e;
+		height: 16px;
+		border-radius: 4px;
+		background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.05));
+		transition: 0.4s linear;
+		transition-property: width, background-color;
+		box-shadow: 0 0 1px 1px rgba(0, 0, 0, 0.25), inset 0 1px rgba(255, 255, 255, 0.1);
+	}
+	
+	.section-5 {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.section-5 .bg {
+		background: url(<?php echo get_template_directory_uri().'/assets/images/img-2.jpg' ?>);
+		background-size: 100%;
+		background-position: center;
+		background-attachment: fixed;
+		position: absolute;
+		top: 0;
+		left: 0;
+		bottom: 0;
+		right: 0;
+		filter: brightness(30%);
+		z-index: -1;
+	}
+	.section-5 h2 {
+		color: #fff;
+	}
+	.section-5 .list {
+		color: #fff;
+		display: flex;
+		justify-content: space-evenly;
+		flex-wrap: wrap;
+	}
+	.section-5 .list-item {
+		width: 30%;
+		display: flex;
+		margin-bottom: 20px;
+		text-align: left;
+		font-size: 20px;
+		font-weight: 600;
+		position: relative;
+	}
+	.section-5 .list .text {
+		text-indent: 30px;
+	}
+	.section-5 .icon {
+		position: absolute;
+		left: 0;
+		top: 0;
+		color: #43a898;
+		font-size: 25px;
+	}
+	
+	.section-6 .image {
+		background-image: url(<?php echo get_template_directory_uri().'/assets/images/spiker.jpg' ?>);
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: contain;
+		width: 100%;
+		height: 820px;
+	}
+	.section-6 .text {
+		position: relative;
+		margin-bottom: 30px;
+	}
+	.section-6 .text:after {
+		content: '';
+		display: block;
+		width: 40px;
+		height: 3px;
+		background: #81164e;
+		position: absolute;
+		bottom: -10px;
+		left: 50%;
+		transform: translateX(-50%);
+	}
+	
+	
+	.section-7 {
+		position: relative;
+		color: #fff;
+	}
+	.section-7 .bg {
+		background: url(<?php echo get_template_directory_uri().'/assets/images/img-2.jpg' ?>);
+		background-size: 100%;
+		background-position: center;
+		background-attachment: fixed;
+		position: absolute;
+		top: 0;
+		left: 0;
+		bottom: 0;
+		right: 0;
+		filter: brightness(30%);
+		z-index: -1;
+	}
+	.section-7 form {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding-top: 30px;
+	}
+	.section-7 form input  {
+		padding-left: 10px;
+		margin-bottom: 10px;
+		width: 350px;
+		height: 40px;
+		background: #fff;
+		outline: none;
+		border: 0;
+		border-radius: 5px;
+		color: #000;
+	}
+	.section-7 input[type="submit"] {
+		background: #81164e;
+		color: #fff;
+	}
+	.section-7 .reg-block {
+		margin-top: 30px;
+		display: flex;
+		align-items: center;
+		background: #43a898;
+		padding: 20px;
+		text-align: left;
+	}
+	.section-7 .icon {
+		font-size: 50px;
+		margin-right: 20px;
+	}
+	.section-7 h4 {
+		font-weight: 600;
+		font-size: 30px;
+	}
+	.section-8 {
+		padding-bottom: 20px;
+	}
+	.section-8 .blocks {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin-bottom: 30px;
+	}
+	.section-8 .info {
+		margin-left: 40px;
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+	}
+	.section-8 .info > div {
+		border-bottom: 1px solid #ccc;
+		width: 100%;
+		text-align: left;
+		padding: 15px 0;
+	}
+	.section-8 .info i {
+		margin-right: 20px;
+		color: #43a898;
+		font-size: 30px;
+	}
+	.section-8 .number {
+		font-size: 25px;
+	}
+	
+	@media (max-width: 768px) {
+		section {
+			padding: 30px 15px;
+		}
+		.section-8 .blocks {
+			flex-direction: column;
+		}
+		.section-8 iframe {
+			width: 100%!important;
+		}
+		.section-5 .list {
+			flex-direction: column;
+		}
+		.section-5 .list-item {
+			width: 100%;
+			flex-direction: column;
+		}
+		.section-7 .reg-block {
+			width: 100%;
+			margin: 30px auto 0;
+			flex-direction: column;
+			text-align: center;
+		}
+		.section-8 .map {
+			width: 100%;
+			margin-bottom: 30px;
+		}
+		.section-8 .info {
+			margin-left: 0;
+		}
+		.section-6 .text+p {
+			width: 100%;
+		}
+		.section-6 .image {
+			max-height: 300px;
+		}
+		.progress-block, .progress {
+			width: 100%;
+		}
+		.section-3 .list {
+			flex-direction: column;
+		}
+		.section-3 .list-item {
+			width: 100%;
+		}
+		.section-3 .image {
+			max-height: 300px;
+		}
+		.section-3 .bg,
+		.section-5 .bg {
+			-webkit-background-size: cover;
+			background-size: cover;
+		}
+		.section-7 form input {
+			width: 100%;
+		}
+		.section-7 h4 {
+			font-size: 20px;
+		}
+		.section-3 .icon {
+			width: 35px;
+			height: 35px;
+		}
+		.navbar-toggle {
+			margin-right: 0!important;
+		}
 	}
 
 </style>
--- wp-content/themes/chmp/navigation.php
+++ wp-content/themes/chmp/navigation.php
@@ -17,7 +17,6 @@
 				<li><a href="#"><?php echo get_theme_mod('menu3') ?></a></li>
 				<li><a href="#"><?php echo get_theme_mod('menu4') ?></a></li>
 				<li><a href="#"><?php echo get_theme_mod('menu5') ?></a></li>
-				<li><i class="fa fa-envelope-o" aria-hidden="true"></i></li>
 			</ul>
 		</div>
 	</div>
@@ -68,32 +67,38 @@ video {
 	padding: 0.5em 1em;
 	text-align: center;
 	border-radius: 0.4em;
+
 }
 
 @media screen and (max-width:768px;){
 	.overlay span {
 		font-size: 25px;
+
 	}
 }
 
 @media screen and (max-width:450px){
 	.overlay span {
 		font-size: 25px;
+
 	}
 }
 
 </style>
-<div id="headerwrap" style="display: block; height:auto;>
-	<div class="container">
+<div id="headerwrap">
+	<div class="container-fluid" style="overflow: hidden; position: relative">
 		<div class="row centred">
 			<div class="video-background">
 				<div class="video-foreground">
-					<div class="section active" id="section0">
+					<div class="section active" id="section0" style="position: relative;">
 						<video playsinline autoplay muted loop>
 							<source src="<?php echo get_template_directory_uri() . '/assets/chmp.mp4' ?>" type="video/mp4">
 						</video>
-						<div class="overlay">
-							<span><?php echo get_theme_mod('block_on_video_text') ?></span>
+						<div class="container">
+							<div class="overlay">
+								
+								<span><?php echo get_theme_mod('block_on_video_text') ?></span>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -101,11 +106,238 @@ video {
 		</div>
 	</div>
 </div>
-
-<div class="row centred">
-	
-	<div id="wrap">
-		adasdasdasd
+<section class="section-2">
+	<div class="container" >
+		<div class="row"  >
+			<h2>
+				КУДА БУДЕМ ПОГРУЖАТЬСЯ?
+			</h2>
+			<p>
+				Будем исследовать подводную часть «айсберга маркетингового процесса»
+			</p>
+		</div>
 	</div>
+</section>
 
-</div>
+
+<section class="section-3">
+	<div class="bg"></div>
+	<div class="container" >
+		<div class="row"  >
+			<h2>РАЗБЕРЁМ ТАКИЕ АКТУАЛЬНЫЕ ВОПРОСЫ, КАК:</h2>
+			<div class="image">
+			
+			</div>
+			<div class="list">
+				<div class="list-item">
+					<div class="icon">
+						<i class="fa fa-adn"></i>
+					</div>
+					<div class="text">3 главных маркетинговых тренда текущего года</div>
+				</div>
+				<div class="list-item">
+					<div class="icon">
+						<i class="fa fa-adn"></i>
+					</div>
+					<div class="text">3 главных маркетинговых тренда текущего года</div>
+				</div>
+				<div class="list-item">
+					<div class="icon">
+						<i class="fa fa-adn"></i>
+					</div>
+					<div class="text">3 главных маркетинговых тренда текущего года</div>
+				</div>
+				<div class="list-item">
+					<div class="icon">
+						<i class="fa fa-adn"></i>
+					</div>
+					<div class="text">3 главных маркетинговых тренда текущего года</div>
+				</div>
+				<div class="list-item">
+					<div class="icon">
+						<i class="fa fa-adn"></i>
+					</div>
+					<div class="text">3 главных маркетинговых тренда текущего года</div>
+				</div>
+				<div class="list-item">
+					<div class="icon">
+						<i class="fa fa-adn"></i>
+					</div>
+					<div class="text">3 главных маркетинговых тренда текущего года</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="section-4">
+	<div class="container" >
+		<div class="row"  >
+			<h2>
+				ЖИВОЙ МАСТЕР-КЛАСС
+			</h2>
+			<div class="blocks">
+				<div class="block">Дата мероприятия</div>
+				<div class="block">29.05.2019</div>
+				<div class="block">Свободных мест</div>
+				<div class="block">15</div>
+			</div>
+			<div class="progress-block">
+				<span>Мест забронировано - 75%</span>
+				<div class="progress">
+					<div class="progress-bar" style="width: 75%;"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="section-5">
+	<div class="bg"></div>
+	<div class="container" >
+		<div class="row"  >
+			<h2>ТРЕНИНГ «МАРКЕТИНГ 3.0 ПОГРУЖЕНИЕ»</h2>
+			<div class="list">
+				<div class="list-item">
+					<div class="icon">
+						<i class="fa fa-adn"></i>
+					</div>
+					<div class="text">Тренды маркетинга - бросить всё и внедрять / подождать / забить?</div>
+				</div>
+				<div class="list-item">
+					<div class="icon">
+						<i class="fa fa-adn"></i>
+					</div>
+					<div class="text">3 главных маркетинговых тренда текущего года</div>
+				</div>
+				<div class="list-item">
+					<div class="icon">
+						<i class="fa fa-adn"></i>
+					</div>
+					<div class="text">3 главных маркетинговых тренда текущего года</div>
+				</div>
+				<div class="list-item">
+					<div class="icon">
+						<i class="fa fa-adn"></i>
+					</div>
+					<div class="text">3 главных маркетинговых тренда текущего года</div>
+				</div>
+				<div class="list-item">
+					<div class="icon">
+						<i class="fa fa-adn"></i>
+					</div>
+					<div class="text">Тренды маркетинга - бросить всё и внедрять / подождать / забить?</div>
+				</div>
+				<div class="list-item">
+					<div class="icon">
+						<i class="fa fa-adn"></i>
+					</div>
+					<div class="text">Тренды маркетинга - бросить всё и внедрять / подождать / забить?</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="section-6">
+	<div class="container">
+		<div class="row">
+			<h2>СПИКЕР</h2>
+			<a target="_blank" href="<?php echo get_template_directory_uri() . '/assets/images/spiker.jpg' ?>">
+				<div class="image"></div>
+			</a>
+			<div class="text">
+				<h3>АЛЕКСЕЙ КОШЕЛЕВ</h3>
+				<span>Генеральный директор Pro Business Center</span>
+			</div>
+			<p>
+				Генеральный директор Центра бизнес-технологий Pro Business Center. Опытный директор проектов / программ, маркетолог, бизнес-аналитик. Сертификат Международной Ассоциации Управления проектами (IPMA), уровень A (Project Director, IPMA® Level А), сертификат Торгово-Промышленной Палаты Украины, квалификационный стандарт Бизнес-тренер (ТПП). Certified ScrumMaster® (Scrum Alliance), Certified Scrum Product Owner® (Scrum Alliance), сертифицированный НЛП-практик. В бизнесе — 20 лет. В маркетинге — 15 лет. В бизнес-обучении — 8 лет. Тренер-консультант UNDP (Программа Развития ООН). Советник президента корпорации Познякижилстрой. Автор и методолог программы инкубации конкурса стартапов «Leadership», ежегодно проводимого Киевским молодежным центром (Киевская городская государственная администрация). Имеет большой опыт реализации девелоперских и маркетинговых проектов, проектов организационного развития, как на национальном, так и на международном уровне. Реализовал и запустил ряд самостоятельных проектов, в т.ч. успешных стартапов, с нуля и под ключ.
+			</p>
+			<div class="social">
+				<a href="#">
+					<i class="fa fa-facebook-square"></i>
+				</a>
+				<a href="#">
+					<i class="fa fa-instagram"></i>
+				</a>
+			</div>
+			
+		</div>
+	</div>
+</section>
+
+
+<section class="section-7">
+	<div class="bg"></div>
+	<div class="container">
+		<div class="row">
+			<h2>
+				ЗАРЕГИСТРИРУЙСЯ СЕЙЧАС.
+			
+			</h2>
+			<span>КОЛИЧЕСТВО МЕСТ ОГРАНИЧЕНО!</span>
+			<form action="">
+				<input type="text" placeholder="Имя">
+				<input type="text" placeholder="Телефон">
+				<input type="submit" value="ЗАБРОНИРОВАТЬ МЕСТО">
+			</form>
+			<div class="reg-block">
+				<div class="icon">
+					<i class="fa fa-money" aria-hidden="true"></i>
+				</div>
+				<div class="text">
+					<h4>РЕГИСТРАЦИОННЫЙ ВЗНОС — 200 грн.
+					</h4>
+					<span>
+						Внесение регистрационного взноса необходимо для бронирования места на мастер-классе.
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="section-8">
+	<div class="container">
+		<div class="row">
+			<h2>
+				МЕСТО ПРОВЕДЕНИЯ
+			</h2>
+			<div class="blocks">
+				<div class="map">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d971.33683187832!2d30.743500439742753!3d46.48127382939259!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c63198cd4bbb4d%3A0xccef805bc260d7f9!2sIl+Decameron+Clubhouse+Odessa!5e0!3m2!1sru!2sua!4v1557413907964!5m2!1sru!2sua"
+							width="700"
+							height="450"
+							frameborder="0"
+							style="border:0"
+							allowfullscreen></iframe>
+				</div>
+				<div class="info">
+					<div class="number">
+						<i class="fa fa-phone"></i>
+						044 499 72 86
+					</div>
+					<div class="adress">
+						<i class="fa fa-map-marker"></i>
+						г. Киев,
+						ул. В. Василевской, 7
+						офис 602
+					</div>
+					<div class="mail">
+						<i class="fa fa-reply"></i>
+						info@probusiness.center
+					</div>
+				</div>
+				
+			</div>
+			<div class="social">
+				<a href="#">
+					<i class="fa fa-facebook-square"></i>
+				</a>
+				<a href="#">
+					<i class="fa fa-instagram"></i>
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
